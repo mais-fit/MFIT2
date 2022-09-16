4dd2dd64be9af9d41cfb52dbb1ae9eaac2972a6e
KauanAmorim <kauanamorim88@gmail.com>
2022-09-15 22:24:01 -0300

    add register of models in admin

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from produtos.models import Clientes, FormasPagamentos, ItensKits, ItensPedidos, Kits, Marmitas, MeiosPagamentos, Pedidos
+
+admin.site.register(Clientes)
+admin.site.register(FormasPagamentos)
+admin.site.register(ItensKits)
+admin.site.register(ItensPedidos)
+admin.site.register(Kits)
+admin.site.register(Marmitas)
+admin.site.register(MeiosPagamentos)
+admin.site.register(Pedidos)
