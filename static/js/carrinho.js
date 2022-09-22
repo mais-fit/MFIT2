3b7ba4cd92408c805bc508e779f514b229464764
AnedinodSantos <anedinodsantos@gmail.com>
2022-09-21 21:07:38 -0300

    update - criado totalizador e funções de adição e remoção de kit

--- static/js/carrinho.js
+++ static/js/carrinho.js
@@ -1,20 +1,72 @@
 let produtos = atualizaIconeCarrinho()
 
 const placeHolder = document.getElementById("placeholder")
-const spanValor = document.getElementById("valor-total")
-let total = 0.00
+const spanValorTotal = document.getElementById("valor-total")
+
+if (totalizaProdutos(produtos) == 0) {
+    spanValorTotal.innerText = "0,00"
+}else{
+    spanValorTotal.innerText = totalizaProdutos(produtos).toString().replace(".", ",")
+}
 
 produtos.forEach(produto => {
-
     renderizaProduto(produto, placeHolder)
 });
 
-spanValor.innerText = total.toString().replace(".", ",")
+const controles = document.querySelectorAll("[data-controle]")
+
+function adicionaKit(inputQtd, kit_id){
+    inputQtd.value = parseInt(inputQtd.value) + 1;
+    produtos.forEach(produto => {
+        if (produto.kit.id == kit_id){
+            produto.kit.qtd += 1
+        }
+    })
+    localStorage.setItem("produtos", JSON.stringify(produtos))
+    spanValorTotal.innerText = totalizaProdutos(produtos).toString().replace(".", ",")
+}
+
+function removeKit(inputQtd, kit_id){
+    // reduz 1 da quantidade de kits
+    inputQtd.value = parseInt(inputQtd.value) - 1
+    produtos.forEach(produto => {
+        if (produto.kit.id == kit_id){
+            produto.kit.qtd -= 1
+        }
+    })
+    localStorage.setItem("produtos", JSON.stringify(produtos))
+    // caso a redução da quantidade chegue a zero excluo o produto
+    if (parseInt(inputQtd.value) == 0){
+        excluiKit(kit_id)
+    }
+    spanValorTotal.innerText = totalizaProdutos(produtos).toString().replace(".", ",")
+}
+
+function excluiKit(kit_id){
+    let novosProdutos = produtos.filter(produto =>{
+        return (produto.kit.id != kit_id) 
+    })
+    localStorage.setItem("produtos", JSON.stringify(novosProdutos))
+    location.reload()
+}
+
+
+function totalizaProdutos(produtos){
+    let total = 0.00
+    produtos.forEach(produto => {
+        const qtdKit = produto.kit.qtd
+        let valorKit = produto.kit.valor
+        valorKit = parseFloat(valorKit.replace(",", "."))
+        total += (qtdKit * valorKit)
+    })
+    return total
+}
 
 
 
-//cria os elementos que html para renderizar produto
+//cria os elementos html para renderizar cada produto (kit/marmitas) da lista
 function renderizaProduto(produto, placeHolder){
+
     const liPrincipal = document.createElement("li")
     liPrincipal.classList.add("list-group-item", "py-3")
 
@@ -25,7 +77,7 @@ function renderizaProduto(produto, placeHolder){
     primeiraDiv.classList.add("col-4", "col-md-3", "col-lg-2")
 
     const img = document.createElement("img")
-    img.setAttribute("src", "") //"static/img/"+produto.kit.link
+    img.setAttribute("src", `static/img/${produto.kit.link}`)
     img.classList.add("img-thumbnail")
 
     primeiraDiv.appendChild(img)
@@ -39,10 +91,16 @@ function renderizaProduto(produto, placeHolder){
     h4.innerText = produto.kit.descricao
 
     const h5 = document.createElement("h5")
-    h5.innerText = `Escolha seu kit e aproveite suas ${produto.kit.qtdMax} refeições.`
+    h5.innerText = `Escolha seu kit e aproveite suas ${produto.kit.qtdMaxMarmitas} refeições.`
+
+    const ancoraDetalhesKit = document.createElement("a")
+    ancoraDetalhesKit.classList.add("text-secondary")
+    ancoraDetalhesKit.setAttribute("href", "#")
+    ancoraDetalhesKit.innerText = "Detalhes do Kit"
 
     segundaDiv.appendChild(h4)
     segundaDiv.appendChild(h5)
+    segundaDiv.appendChild(ancoraDetalhesKit)
 
 
     const terceiraDiv = document.createElement("div")
@@ -54,25 +112,60 @@ function renderizaProduto(produto, placeHolder){
     const groupBtn = document.createElement("div")
     groupBtn.classList.add("input-group")
 
+    const btnMenos = document.createElement("button")
+    btnMenos.setAttribute("type", "button")
+    btnMenos.setAttribute("data-controle", "removeKit")
+    btnMenos.classList.add("btn", "btn-outline-dark", "btn-sm", "bi-caret-down")
+    btnMenos.onclick = evento => {
+        const operacao = evento.target.dataset.controle
+        const controle = evento.target.parentNode
+        const inputQtd = controle.querySelector("#qtd")
+        window[operacao](inputQtd, produto.kit.id)
+    }
+
+    const inputQtd = document.createElement("input")
+    inputQtd.setAttribute("type", "text")
+    inputQtd.setAttribute("disabled", "")
+    inputQtd.setAttribute("id", "qtd")
+    inputQtd.classList.add("form-control", "text-center", "border-dark")
+    inputQtd.value = produto.kit.qtd
+
+
+    const btnMais = document.createElement("button")
+    btnMais.setAttribute("type", "button")
+    btnMais.setAttribute("data-controle", "adicionaKit")
+    btnMais.classList.add("btn", "btn-outline-dark", "btn-sm", "bi-caret-up")
+    btnMais.onclick = evento => {
+        const operacao = evento.target.dataset.controle
+        const controle = evento.target.parentNode
+        const inputQtd = controle.querySelector("#qtd")
+        window[operacao](inputQtd, produto.kit.id)
+    }
+
     const btnLixeira = document.createElement("button")
     btnLixeira.setAttribute("type", "button")
     btnLixeira.classList.add("btn", "btn-outline-danger", "border-dark", "btn-sm")
+    btnLixeira.onclick = () => {
+
+        excluiKit(produto.kit.id)
+    }
 
     const iconeLixeira = document.createElement("i")
     iconeLixeira.setAttribute("style", "font-size: 16px; line-height: 16px;")
     iconeLixeira.classList.add("bi-trash")
 
     btnLixeira.appendChild(iconeLixeira)
+
+    groupBtn.appendChild(btnMenos)
+    groupBtn.appendChild(inputQtd)
+    groupBtn.appendChild(btnMais)
     groupBtn.appendChild(btnLixeira)
 
-    // <div class="text-end mt-2">
-    //     <span class="text-dark">Valor Kit: R$ </span>
-    // </div>
     const divValor = document.createElement("div")
     divValor.classList.add("text-end", "mt-2")
 
     const spanValor = document.createElement("span")
-    spanValor.classList.add("text-dark")
+    spanValor.classList.add("text-secondary")
     spanValor.innerText = "Valor Kit: R$ " + produto.kit.valor
 
     divValor.appendChild(spanValor)
@@ -89,6 +182,5 @@ function renderizaProduto(produto, placeHolder){
 
     placeHolder.appendChild(liPrincipal)
 
-    let valor = produto.kit.valor.replace(",",".")
-    total += parseFloat(valor)
+    totalizaProdutos(produtos)
 }
